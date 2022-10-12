f3c322f19e1981628cd9f884e04e8ebeb1d0aff8
Monjur <jacosmonjurajad@gmail.com>
2022-10-12 15:46:38 +0600

    sms system done

---
+++ app/Lib/Adnsms/config/config.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!defined('API_DOMAIN_URL')) {
+  define('API_DOMAIN_URL', 'https://portal.adnsms.com');
+}
+
+if (!defined('API_KEY')) {
+  define('API_KEY', 'KEY-zqwi8gt7pqamo52rk4nnz7ipxj2j4yb9');
+}
+
+if (!defined('API_SECRET')) {
+  define('API_SECRET', 'mUCC58ygOoH2LlI4');
+}
+
+return [
+    'domain' => constant("API_DOMAIN_URL"),
+    'apiCredentials' => [
+        'api_key' => constant("API_KEY"),
+        'api_secret' => constant("API_SECRET"),
+    ],
+    'apiUrl' => [
+        'check_balance' => "/api/v1/secure/check-balance",
+        'send_sms' => "/api/v1/secure/send-sms",
+        'check_campaign_status' => "/api/v1/secure/campaign-status",
+        'check_sms_status' => "/api/v1/secure/sms-status",
+    ],
+];
